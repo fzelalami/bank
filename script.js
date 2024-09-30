6081f5040e63a02d728a335e427017f334abe07c
EL ALAMI FATIMA ZAHRA <fzelalami@device-471.home>
2024-09-30 13:38:26 +0200

    Implementing Smooth Scrolling

--- script.js
+++ script.js
@@ -29,3 +29,12 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+// Implementing Smooth Scrolling
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener("click", function (e) {
+  // Scrolling
+  section1.scrollIntoView({ behavior: "smooth" });
+});
