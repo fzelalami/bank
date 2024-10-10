356d4cfeb4b19ba8321075bb69f65192cfed516b
EL ALAMI FATIMA ZAHRA <fzelalami@device-471.home>
2024-10-10 05:37:16 +0200

    Reveal Sections

--- script.js
+++ script.js
@@ -105,3 +105,26 @@ const StickyNav = function (entries) {
 const headerObserver = new IntersectionObserver(StickyNav, options);
 
 headerObserver.observe(header);
+
+///////////////////////////////////////////////
+// Reveal Sections
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
